2ed0a64bd3ad9d4f9126b34242d1b668953e5eb5
John Chadwick <john@jchw.io>
2022-07-04 13:22:34 -0400

    Fix install prefix logic.

--- Client/main.h
+++ Client/main.h
@@ -4,12 +4,10 @@
 #ifndef PREFIX
 #   if defined(Q_OS_WIN)
 #       define PREFIX "C:\\Program Files\\PuyoVS"
-#   elif defined(Q_OS_MAC) && defined(__x86_64__)
-#       define PREFIX "/usr/local"
 #   elif defined(Q_OS_MAC) && defined(__aarch64__)
 #       define PREFIX "/opt/homebrew"
 #   else
-#       define PREFIX "/usr"
+#       define PREFIX "/usr/local"
 #   endif
 #endif
 
