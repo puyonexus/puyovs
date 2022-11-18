6f1c13b971f2a83b2ce07c0ad98c369f3a693432
Dohyeon Jeon <jdh59681@naver.com>
2022-11-18 09:18:03 +0900

    Use BuildTools and RelatePath Instead
    * Building with BuildTools works just fine
    * Using relatepath will prevent it to git clone again and again
      * Using relatepath will prevent you to install git
    * Set Qt path to relate path. Defaults to "C:\", but you can change it.

--- BuildScripts/WindowsPowershell/common_build_powershell.ps1
+++ BuildScripts/WindowsPowershell/common_build_powershell.ps1
@@ -1,4 +1,4 @@
-﻿echo "Puyo VS Build Script"
+echo "Puyo VS Build Script"
 echo ""
 echo "DO NOT USE THIS AS ARM64 BUILD: THE BUILD WILL CRASH!!!!!"
 echo ""
@@ -10,7 +10,6 @@ echo "Before start, you SHOULD install these programs manually."
 echo "If installed, press ENTER to continue."
 echo ""
 echo "Programs:"
-echo "  - Git"
 echo "  - 7z"
 echo "  - cmake"
 echo ""
@@ -28,65 +27,68 @@ if ( [IntPtr]::Size -eq 8 ) {
     $MATARCH = 'x64'
 }
 
-$GITHUB_PATH = "$env:USERPROFILE\GitHub\PuyoVS"
-del "$GITHUB_PATH" -Recurse -Force
+$GitHubPath = ($MyInvocation.MyCommand).Path
+for ($i = 0; $i -le 3; $i++) {
+	Split-Path $GitHubPath -Parent
+}
+
+$QtPath = "C:\"
 
 ### Phase 1. Install winget if not installed
-###### (If just using Qt and cmake, this could be skipped)
-###### actually it foeces to download the latest one and force-install it
 echo "Downloading and installing winget..."
 ## Download from aka.ms...
 Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile "$env:TEMP\PuyoVS_Build\winget.msixbundle"
 ## Force-install... (If newer one installed, it'll be crashed with code 0)
 Add-AppPackage -path "$env:TEMP\PuyoVS_Build\winget.msixbundle"
-## ..and install VS 2022
-winget install Microsoft.VisualStudio.2022.Community --silent --override "--wait --quiet --add ProductLang En-us --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended"
+echo "Winget installed successfully."
+echo ""
+
+### Phase 1.1. Install Visual Studio BuildTools 2022
+## Install with winget...
+winget install Microsoft.VisualStudio.2022.BuildTools --silent --override "--wait --quiet --add ProductLang En-us --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended"
 
 ### Phase 2. Download Qt SDK
 clear
-echo "Winget installed successfully."
+echo "Visual Studio BuildTools has been installed successfully."
 echo ""
 echo "Now this will download Qt SDK, required to build Puyo VS."
 ## Download from GitHub...
-Invoke-WebRequest -Uri "https://github.com/puyonexus/qt-sdk-builder/releases/download/v2/qt-windows-5.15.2-$MATARCH-msvc-static-release.7z" -OutFile "C:\qt.7z"
+Invoke-WebRequest -Uri "https://github.com/puyonexus/qt-sdk-builder/releases/download/v2/qt-windows-5.15.2-$MATARCH-msvc-static-release.7z" -OutFile "$QtPath\qt.7z"
 
 ### Phase 3. Extract Qt SDK
 echo "Extracting..."
 echo "If error occurs, please install 7z and run the script from the beginning."
 ## Use 7z to extract qt.7z
-7z x C:\qt.7z -oC:\Qt\
+7z x "$QtPath\qt.7z" -o"$QtPath\Qt\"
 ## ..and add to path
 #### Quote: https://techexpert.tips/powershell/powershell-edit-path-variable/
-$TEMPQT = "C:\Qt\Qt-5.15.2\bin"
+$TEMPQT = "$QtPath\Qt\Qt-5.15.2\bin"
 $OLDPATH = [System.Environment]::GetEnvironmentVariable('PATH','machine')
-$NEWPATH = "$OLDPATH;$TEMP_CMAKE;$TEMPQT"
+$NEWPATH = "$OLDPATH;$TEMPQT"
 [Environment]::SetEnvironmentVariable('PATH', "$NEWPATH", 'MACHINE')
 #### Cleanup
 $TEMPQT = ""
 $OLDPATH = ""
 $NEWPATH = ""
 
-### Phase 4. Git Clone
-clear
-echo "Now this will clone Puyo VS repository."
-echo "Ignore the git error."
-git clone --recursive "https://github.com/puyonexus/puyovs.git" "$GITHUB_PATH"
-cd "$GITHUB_PATH"
-
-### Phase 5. Make CMake Build Directory
+### Phase 4. Make CMake Build Directory
 clear
 echo "Now this will configure PuyoVS via CMake."
+## Relate 
+cd $GitHubPath
+mkdir .\.build
 ## Init CMake
 cmake -B .build
 ## Build PuyoVS
 cmake --build .build --parallel 8 --config RelWithDebInfo
 ## Install Assets
 cmake --install .build --config RelWithDebInfo
+echo "Build finished. Please check file at '$GitHubPath\.build\Client\RelWithDebInfo\PuyoVS.exe'."
 pause
 
 ### Phase 6. Finalizing
 cls
 echo "Build finished. Cleaning up..."
-del C:\qt -Recurse -Force
-del C:\qt.7z
+del "$QtPath\qt" -Recurse -Force
+del "$QtPath\qt.7z"
 pause
